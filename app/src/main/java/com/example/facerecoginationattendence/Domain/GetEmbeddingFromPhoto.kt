099655a77968c6e7a2edcf5e4f150f4d07594fb4
kamal gond <kamalgond0@gmail.com>
2025-11-29 15:40:56 +0530

    added comment and changed

--- app/src/main/java/com/example/facerecoginationattendence/Domain/GetEmbeddingFromPhoto.kt
+++ app/src/main/java/com/example/facerecoginationattendence/Domain/GetEmbeddingFromPhoto.kt
@@ -6,67 +6,13 @@ import org.tensorflow.lite.support.image.ImageProcessor
 import org.tensorflow.lite.support.image.TensorImage
 import org.tensorflow.lite.support.image.ops.ResizeOp
 
-//package com.example.facerecoginationattendence.Domain
-//
-//import android.graphics.Bitmap
-//import android.graphics.Color
-//import android.util.Log
-//import kotlinx.coroutines.Dispatchers
-//import kotlinx.coroutines.channels.awaitClose
-//import kotlinx.coroutines.flow.Flow
-//import kotlinx.coroutines.flow.callbackFlow
-//import kotlinx.coroutines.withContext
-//import org.tensorflow.lite.Interpreter
-//import kotlin.math.sqrt
-//
-//suspend fun getEmbeddingFromBitmap(
-//    bitmap: Bitmap,
-//    interpreter: Interpreter,
-//    inputSize: Int = 112
-//): FloatArray = withContext(Dispatchers.Default) {
-//
-//    val resized = Bitmap.createScaledBitmap(bitmap, inputSize, inputSize, true)
-//
-//    // Correct input tensor shape [1,112,112,3]
-//    val input = Array(1) { Array(inputSize) { Array(inputSize) { FloatArray(3) } } }
-//
-//    for (y in 0 until inputSize) {
-//        for (x in 0 until inputSize) {
-//            val pixel = resized.getPixel(x, y)
-//            input[0][y][x][0] = Color.red(pixel) / 255f
-//            input[0][y][x][1] = Color.green(pixel) / 255f
-//            input[0][y][x][2] = Color.blue(pixel) / 255f
-//        }
-//    }
-//
-//    // Output: 192-dim
-//    val output = Array(1) { FloatArray(192) }
-//
-//    // Run TFLite
-//    interpreter.run(input, output)
-//
-//    Log.d("raw",output.joinToString(","))
-//
-//    // Normalize and return
-//    l2Normalize(output[0])
-//}
-//
-//
-//// Helper: Normalize vector
-//fun l2Normalize(vector: FloatArray): FloatArray {
-//    var sum = 0f
-//    for (v in vector) sum += v * v
-//    val norm = sqrt(sum)
-//    val out = FloatArray(vector.size)
-//    for (i in vector.indices) out[i] = vector[i] / norm
-//    return out
-//}
+// function to get embedding
 fun getEmbeddingFromBitmap(bitmap: Bitmap, interpreter: Interpreter): FloatArray {
     // Step 1: Create a TensorImage object from the bitmap.
     var tensorImage = TensorImage.fromBitmap(bitmap)
 
     // Step 2: Create an ImageProcessor to define the preprocessing steps.
-    // MobileFaceNet expects a 112x112 image.
+    // FaceNet expects a 160x160 image.
     // The pixel values should be normalized from [0, 255] to [-1, 1].
     val imageProcessor = ImageProcessor.Builder()
         .add(ResizeOp(160, 160, ResizeOp.ResizeMethod.BILINEAR))
@@ -78,7 +24,7 @@ fun getEmbeddingFromBitmap(bitmap: Bitmap, interpreter: Interpreter): FloatArray
     tensorImage = imageProcessor.process(tensorImage)
 
     // Step 4: Create the output buffer for the embedding.
-    // MobileFaceNet produces a 192-element embedding.
+
     val embedding = Array(1) { FloatArray(128) }
 
     // Step 5: Run inference.
@@ -87,6 +33,8 @@ fun getEmbeddingFromBitmap(bitmap: Bitmap, interpreter: Interpreter): FloatArray
     // Step 6: Return the embedding.
     return l2Normalize(embedding[0])
 }
+
+//function to normalize it so that it become comparavble
 fun l2Normalize(embedding: FloatArray): FloatArray {
     var sum = 0f
     for (v in embedding) sum += v * v
